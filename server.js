b50393793f495fe4a1068180223d25216e885621
oluwadaprof <adeekotobiloba8@gmail.com>
2022-08-30 11:58:31 +0100

    server public folder files

--- server.js
+++ server.js
@@ -3,6 +3,19 @@ const app = express();
 const path = require("path");
 const PORT = process.env.PORT || 3500;
 
+
+//buil-in middleware to handle urlencoded data
+//in other words, form data:
+//'content-type: application/x-www-form-urlencoded'
+app.use(express.urlencoded({extended:false}))
+
+// built-in middleware for json
+app.use(express.json())
+
+//server static files
+app.use(express.static(path.join(__dirname, '/public')))
+
+
 app.get("^/$|/index(.html)?", function (req, res) {
   // res.sendFile('./views/index.html' , {root: __dirname});
   res.sendFile(path.join(__dirname, "views", "index.html"));
