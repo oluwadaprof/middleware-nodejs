a52e8b1e58fd8908fcbdb9551ae0fd4d0fbab631
oluwadaprof <adeekotobiloba8@gmail.com>
2022-08-31 09:20:26 +0100

    finished middleware logevent

--- server.js
+++ server.js
@@ -1,13 +1,29 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const cors = require('cors')
 const {logEvents, logger }= require('./middleware/logEvents')
+const errorHandler = require('./middleware/errorHandler')
 const PORT = process.env.PORT || 3500;
 
 
 
 //custom middleware logger
 app.use(logger)
+
+// Cross Origin Resource Sharing
+const whitelist = ['https://www.google.com', 'http://127.0.0:5500', 'http://localhost:3500']
+const corsOption = {
+  origin: (origin, callback)=> {
+    if (whitelist.indexOf(origin) !== -1 || !origin){
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  },
+  optionsSuccessStatus: 200
+}
+app.use(cors(corsOption))
 
 //buil-in middleware to handle urlencoded data
 //in other words, form data:
@@ -30,6 +46,8 @@ app.get("/new-page(.html)?", function (req, res) {
   // res.sendFile('./views/new-page.html' , {root: __dirname});
   res.sendFile(path.join(__dirname, "views", "index.html"));
 });
+
+
 
 app.get("/old-page(.html)?", function (req, res) {
   res.redirect(301, "new-page.html");
@@ -65,8 +83,19 @@ const three = (res, req ) => {
 
 app.get('/chain(.html)?', [one, two, three])
 
-app.get("/*", function (req, res) {
-  res.status(404).sendFile(path.join(__dirname), "views", "404.html");
+app.all("*", function (req, res) {
+  res.status(404);
+   
+  if (req.accepts('html')){
+    res.sendFile(path.join(__dirname), "views", "404.html")
+  }else if (req.accepts('json')){
+    res.json({error: '404 Not Found'})
+  } else {
+    res.type('txt').send('404 Not Found')
+  }
 });
+
+
+app.use(errorHandler)
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
